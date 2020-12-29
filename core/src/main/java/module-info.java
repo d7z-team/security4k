a229fcc321e93671a999e1fe3007775683cde58d
dragon <ExplodingFKL@gmail.com>
2020-12-29 10:31:19 +0800

    去除无用依赖

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module security4k.core {
     requires kotlin.reflect;
     requires kotlin.stdlib;
-    requires logger4k.core;
     opens com.github.open_edgn.security4k.base64;
     exports com.github.open_edgn.security4k.base64;
+    opens com.github.open_edgn.security4k.hash;
+    exports com.github.open_edgn.security4k.hash;
     opens com.github.open_edgn.security4k.asymmetric.rsa;
     exports com.github.open_edgn.security4k.asymmetric.rsa;
 }
